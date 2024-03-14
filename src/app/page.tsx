8f06a0b0b7e08ad0e60910cf1dd26b3578e7e62f
Igor Bedesqui <igorbedesqui@gmail.com>
2024-03-14 01:07:44 +0000

    ehhh I'm tired and got distracted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,34 @@
 "use client";
 import { drawer } from "@/components/drawer";
 
+const possible_exercises = [
+  "walking lunges",
+  "push-ups",
+  "rows",
+  "plank shoulder taps",
+];
+
 export default function Home() {
   return (
     <>
       <main className="container">
-        <h1>Bare minimum</h1>
+        <h1 className="hidden">Bare minimum</h1>
 
-        <button
-          onClick={() => {
-            drawer({ content: <div>Drawer content</div> });
-          }}
-        >
-          Open Drawer
-        </button>
+        <section className="flex justify-center">
+          <button className="h-min">
+            <span className="text-8xl leading-none">01</span>
+          </button>
+        </section>
+
+        <section>
+          <button
+            onClick={() => {
+              drawer({ content: <div>drawer content</div> });
+            }}
+          >
+            +
+          </button>
+        </section>
       </main>
     </>
   );
